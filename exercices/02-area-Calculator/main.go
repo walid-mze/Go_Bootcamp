30e10e761d0670548604a4ca492f66ad5d765016
WalidMamze <Walid.MAMZE@um6p.ma>
2026-01-05 15:31:24 +0100

    part3

--- exercices/02-area-Calculator/main.go
+++ exercices/02-area-Calculator/main.go
@@ -7,9 +7,11 @@ func calculateArea(length float64, width float64) float64 {
 		log.Println("length and width must be positive values")
 	}
 	return length * width
+
 }
 func main() {
 	area := calculateArea(5.0, 3.0)
 	log.Println("Area in meters :", area)
 	log.Println("Area in foot :", area*3.28*3.28)
+	
 }
