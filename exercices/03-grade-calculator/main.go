73ad0a73e39a28c5aa76abbbdff0d880debf31dc
WalidMamze <Walid.MAMZE@um6p.ma>
2026-01-05 15:36:36 +0100

    part 1&2

--- exercices/03-grade-calculator/main.go
+++ exercices/03-grade-calculator/main.go
@@ -4,21 +4,28 @@ import "fmt"
 
 func main(){
 	var g1 int
+	var g2 int
+	var g3 int
+	var g4 int
+	var g5 int
+
+	fmt.Print("Enter the first value: ")
+	fmt.Scanf("%d",&g1)
+
+	fmt.Print("Enter the second value: ")
+	fmt.Scanf("%d",&g2)
+
+	fmt.Print("Enter the third value: ")
+	fmt.Scanf("%d",&g3)
+
+	fmt.Print("Enter the fourth value: ")
+	fmt.Scanf("%d",&g4)
+
+	fmt.Print("Enter the fifth value: ")
+	fmt.Scanf("%d",&g5)
 
 
-	i:=0
-	sum:=0
-	for{
-		
-		fmt.Print("enter next value: ");
-		fmt.Scanf("%d",&g1)
-		if g1==-1{
-			break
-		}
-		sum+=g1
-		i++;
-	}
-	avg:=(float64)(sum)/5
+	avg:=(float64)(g1+g2+g3+g4+g5)/5
 	fmt.Println("the average grade is",avg)
 
 
