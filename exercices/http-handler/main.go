7a081bff438d71064e1f8a352891b3e9f1589efd
WalidMamze <Walid.MAMZE@um6p.ma>
2026-01-08 14:33:08 +0100

    ex9

--- exercices/http-handler/main.go
+++ exercices/http-handler/main.go
@@ -1,7 +1,27 @@
 package main
-import (
-"net/http")
 
-func main(){
-	http.ListenAndServe(":3000",nil)
+import (
+	"fmt"
+	"net/http"
+	"io"
+	"log"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello World")
+}
+
+func main() {
+	resp,err:=http.Get("http://localhost:3000/")
+	if err!=nil{
+		log.Fatal(err)
+	}
+	message,err:=io.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err!=nil{
+		log.Fatal(err)
+	}
+	fmt.Printf("%s",message)
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":3000", nil)
 }
